7679c6492197542bac3a10d7f9d71ccf5f1e308d
ColtanF <coltan.franke@gmail.com>
2021-06-29 11:32:30 -0500

    Added rejections page/db column.

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ app.debug = True
 # Config MySql
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'qwerQWER1234!@#$' # Change this to w/e your db password is
+# Change this to w/e your db password is
+app.config['MYSQL_PASSWORD'] = 'qwerQWER1234!@#$'
 app.config['MYSQL_DB'] = 'job_tracker_db'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
@@ -20,6 +21,8 @@ app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 mysql = MySQL(app)
 
 # Check if user logged in
+
+
 def isLoggedIn(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -30,12 +33,14 @@ def isLoggedIn(f):
             return redirect(url_for('login'))
     return wrap
 
+
 @app.route('/')
 def index():
     jobs = []
     if 'logged_in' in session:
         cur = mysql.connection.cursor()
-        result = cur.execute("SELECT * FROM jobs_tbl WHERE username = %s", [session["username"]])
+        result = cur.execute(
+            "SELECT * FROM jobs_tbl WHERE (username = %s AND rejected = False)", [session["username"]])
 
         jobs = list(cur.fetchall())
         jobs.sort(key=lambda x: x['rating'])
@@ -45,6 +50,7 @@ def index():
         cur.close()
     return render_template('index.html', jobs=jobs)
 
+
 @app.route('/jobs')
 @isLoggedIn
 def jobs():
@@ -52,7 +58,8 @@ def jobs():
     cur = mysql.connection.cursor()
 
     # Grab all of the jobs
-    result = cur.execute("SELECT * FROM jobs_tbl WHERE username = %s", [session["username"]])
+    result = cur.execute(
+        "SELECT * FROM jobs_tbl WHERE (username = %s AND rejected = False)", [session["username"]])
     jobs = cur.fetchall()
 
     cur.close()
@@ -60,17 +67,34 @@ def jobs():
     return render_template('jobs.html', jobs=jobs)
 
 
+@app.route('/rejects')
+@isLoggedIn
+def rejects():
+    # Retrieve jobs postings from which I've been rejected.
+    cur = mysql.connection.cursor()
+
+    # Grab the jobs where rejected=True
+    result = cur.execute(
+        "SELECT * FROM jobs_tbl WHERE (username = %s and rejected = TRUE)", [session["username"]])
+    jobs = cur.fetchall()
+    cur.close()
+    return render_template('jobs.html', jobs=jobs, rejects=True)
+
+
 class JobForm(Form):
     company = StringField('Company Name', [validators.Length(min=1, max=100)])
     position = StringField('Position', [validators.Length(min=10, max=200)])
-    companyInfo = TextAreaField('Company Description', [validators.Length(min=1)])
-    positionInfo = TextAreaField('Position Description', [validators.Length(min=1)])
+    companyInfo = TextAreaField('Company Description', [
+                                validators.Length(min=1)])
+    positionInfo = TextAreaField('Position Description', [
+                                 validators.Length(min=1)])
     reqsIMeet = TextAreaField('Requirements I meet')
     reqsIDontMeet = TextAreaField('Requirements I don\'t meet')
     address = StringField('Address', [validators.Length(min=1, max=300)])
     links = TextAreaField('Links')
     salary = StringField('Salary', [validators.Length(max=200)])
     status = StringField('Status', [validators.Length(min=1, max=200)])
+
 
 @app.route('/add_job', methods=["GET", "POST"])
 @isLoggedIn
@@ -91,7 +115,7 @@ def add_job():
         # Add the job to the DB
         cur = mysql.connection.cursor()
 
-        cur.execute("INSERT INTO jobs_tbl(company, position, companyInfo, positionInfo, reqsIMeet, reqsIDontMeet, salary, address, links, status, username, rating) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 0)", 
+        cur.execute("INSERT INTO jobs_tbl(company, position, companyInfo, positionInfo, reqsIMeet, reqsIDontMeet, salary, address, links, status, username, rating, rejected) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 0, false)",
                     (company, position, companyInfo, positionInfo, reqsIMeet, reqsIDontMeet, salary, address, links, status, session["username"]))
 
         mysql.connection.commit()
@@ -99,8 +123,9 @@ def add_job():
         cur.close()
         flash("Job added.", "success")
         return redirect(url_for("jobs"))
-        
+
     return render_template('add_job.html', form=form)
+
 
 @app.route('/delete_job/<string:id>', methods=["POST"])
 @isLoggedIn
@@ -114,6 +139,21 @@ def delete_job(id):
     flash("Job deleted.", "success")
     return redirect(url_for('jobs'))
 
+
+@app.route('/rejected/<string:id>', methods=["POST"])
+@isLoggedIn
+def rejected(id):
+    # Set rejection flag to true in the db
+    cur = mysql.connection.cursor()
+    result = cur.execute(
+        "UPDATE jobs_tbl SET rejected = True WHERE id = %s", [id])
+    mysql.connection.commit()
+    cur.close()
+
+    flash("Job moved to rejected. You'll get 'em next time!", "success")
+    return redirect(url_for("jobs"))
+
+
 @app.route('/view_job/<string:id>', methods=['GET', 'POST'])
 @isLoggedIn
 def view_job(id):
@@ -122,12 +162,13 @@ def view_job(id):
     result = cur.execute("SELECT * FROM jobs_tbl WHERE id = %s", [id])
     job = cur.fetchone()
     cur.close()
-    if request.method == "POST": 
+    if request.method == "POST":
         # rating must have been updated. Update the rating in the db
         ratingsForm = request.form
         if ratingsForm['stars']:
             cur = mysql.connection.cursor()
-            result = cur.execute("UPDATE jobs_tbl SET rating = %s WHERE id = %s", [ratingsForm['stars'], id])
+            result = cur.execute("UPDATE jobs_tbl SET rating = %s WHERE id = %s", [
+                                 ratingsForm['stars'], id])
             mysql.connection.commit()
             result = cur.execute("SELECT * FROM jobs_tbl WHERE id = %s", [id])
             job = cur.fetchone()
@@ -136,8 +177,8 @@ def view_job(id):
 
         return render_template('view_job.html', job=job)
 
+    return render_template('view_job.html', job=job)
 
-    return render_template('view_job.html', job = job)
 
 @app.route('/edit_job/<string:id>', methods=['GET', 'POST'])
 @isLoggedIn
@@ -172,14 +213,13 @@ def edit_job(id):
         status = request.form["status"]
         links = request.form["links"]
 
-
      # Add the job to the DB
         cur = mysql.connection.cursor()
 
-        cur.execute("UPDATE jobs_tbl SET company = %s, position = %s, companyInfo = %s, positionInfo = %s, reqsIMeet = %s, reqsIDontMeet = %s, salary = %s, address = %s, links = %s, status = %s WHERE id = %s", 
+        cur.execute("UPDATE jobs_tbl SET company = %s, position = %s, companyInfo = %s, positionInfo = %s, reqsIMeet = %s, reqsIDontMeet = %s, salary = %s, address = %s, links = %s, status = %s WHERE id = %s",
                     (company, position, companyInfo, positionInfo, reqsIMeet, reqsIDontMeet, salary, address, links, status, id))
         mysql.connection.commit()
-        
+
         # Grab the updated job
         result = cur.execute("SELECT * FROM jobs_tbl WHERE id = %s", [id])
         job = cur.fetchone()
@@ -191,6 +231,7 @@ def edit_job(id):
 
     return render_template('edit_job.html', job=job, form=form)
 
+
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     username = StringField('Username', [validators.Length(min=4, max=25)])
@@ -201,7 +242,8 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
-@app.route('/register', methods = ["POST", "GET"])
+
+@app.route('/register', methods=["POST", "GET"])
 def register():
     form = RegisterForm(request.form)
     if request.method == "POST" and form.validate():
@@ -218,11 +260,13 @@ def register():
         mysql.connection.commit()
         data = cur.fetchall()
         if len(data) != 0:
-            flash("Account with username " + username + " already exists, please choose a different username.", "warning")
+            flash("Account with username " + username +
+                  " already exists, please choose a different username.", "warning")
             return render_template('register.html', form=form)
 
         # If the username is unique, add it to the database
-        cur.execute("INSERT INTO users(name,email,username,password) VALUES(%s, %s, %s, %s)", (name, email, username, password))
+        cur.execute("INSERT INTO users(name,email,username,password) VALUES(%s, %s, %s, %s)",
+                    (name, email, username, password))
 
         # Commit to db
         mysql.connection.commit()
@@ -232,6 +276,7 @@ def register():
 
         return redirect(url_for('index'))
     return render_template('register.html', form=form)
+
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
@@ -245,7 +290,8 @@ def login():
         cur = mysql.connection.cursor()
 
         # Check for username
-        result = cur.execute("SELECT * FROM users WHERE username = %s", [username])
+        result = cur.execute(
+            "SELECT * FROM users WHERE username = %s", [username])
 
         if result > 0:
             # Get stored hash
@@ -266,13 +312,14 @@ def login():
                 error = "Invalid password."
                 app.logger.info(error)
                 return render_template('login.html', error=error)
-        else: 
+        else:
             # invalid username
             error = "Invalid username."
             app.logger.info(error)
             return render_template('login.html', error=error)
 
     return render_template("login.html")
+
 
 @app.route('/logout')
 @isLoggedIn
@@ -281,7 +328,8 @@ def logout():
     flash("You are now logged out.", "success")
     return redirect(url_for('login'))
 
+
 if __name__ == "__main__":
     mysql_db_helper.checkAndMakeDB()
-    app.secret_key = "123abc" # Change this to something more secure
+    app.secret_key = "123abc"  # Change this to something more secure
     app.run(port=5004)
